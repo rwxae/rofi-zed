ac73aced9ac5ae85eb240cce69059cb737c63391
wardxela <wardxela@gmail.com>
2026-04-05 02:05:43 +0300

    feat: here's actual implementation xd

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
-use std::process::Command;
+use itertools::Itertools;
+use std::{fs, io, path::PathBuf, process::Command};
 
 use rofi_mode::Event;
+use rusqlite::{Connection, OpenFlags, fallible_iterator::FallibleIterator};
 
 pub struct ZedMode<'rofi> {
     api: rofi_mode::Api<'rofi>,
@@ -9,14 +11,14 @@ pub struct ZedMode<'rofi> {
 
 enum Entry {
     SearchRecents,
-    OpenProject(String),
+    Open(String),
 }
 
 impl Entry {
     fn as_str(&self) -> &str {
         match self {
             Self::SearchRecents => "Search Recent Projects",
-            Self::OpenProject(project) => project.as_str(),
+            Self::Open(project) => project.as_str(),
         }
     }
 }
@@ -25,19 +27,40 @@ fn get_menu_entries() -> Vec<Entry> {
     vec![Entry::SearchRecents]
 }
 
-// TODO: fetch real paths
-fn get_recent_projects() -> Vec<Entry> {
-    vec![
-        Entry::OpenProject("/home/rwxae/code/dotfiles".to_string()),
-        Entry::OpenProject("/home/rwxae/code/stylix".to_string()),
-    ]
+// https://zed.dev/docs/troubleshooting?highlight=0-stable#startup-and-workspace-issues
+const ZED_DB_CHANNELS: [&str; 4] = ["0-stable", "0-preview", "0-nightly", "0-dev"];
+
+fn query_recent_projects(db: &PathBuf) -> rusqlite::Result<Vec<String>> {
+    Connection::open_with_flags(db, OpenFlags::SQLITE_OPEN_READ_ONLY)?
+        .prepare(
+            "SELECT w.paths FROM workspaces w
+            WHERE w.paths IS NOT NULL AND length(w.paths) > 0
+            ORDER BY w.timestamp DESC",
+        )?
+        .query([])?
+        .map(|row| row.get::<_, String>(0))
+        .collect::<Vec<String>>()
 }
 
-fn open_zed(path: &str) {
-    Command::new("zeditor")
-        .arg(path)
-        .spawn()
-        .expect("zeditor is not found");
+fn get_recent_projects() -> Result<Vec<Entry>, &'static str> {
+    let zed_db_dir = dirs::data_dir()
+        .map(|path| path.join("zed/db"))
+        .ok_or("Unable to determine a path to $XDG_DATA_HOME")?;
+    let entries = ZED_DB_CHANNELS
+        .iter()
+        .map(|channel| zed_db_dir.join(channel).join("db.sqlite"))
+        .filter(|path| path.try_exists().unwrap_or(false))
+        .filter_map(|path| query_recent_projects(&path).ok())
+        .flatten()
+        .unique_by(|project| project.clone())
+        .filter(|project| fs::exists(project).unwrap_or(false))
+        .map(|project| Entry::Open(project))
+        .collect();
+    Ok(entries)
+}
+
+fn open_zed(path: &str) -> io::Result<std::process::Child> {
+    Command::new("zeditor").arg(path).spawn()
 }
 
 impl<'rofi> rofi_mode::Mode<'rofi> for ZedMode<'rofi> {
@@ -64,33 +87,39 @@ impl<'rofi> rofi_mode::Mode<'rofi> for ZedMode<'rofi> {
         match &event {
             Event::Ok { selected, .. } => match &self.entries[*selected] {
                 Entry::SearchRecents => {
-                    self.entries = get_recent_projects();
-                    return rofi_mode::Action::Reload;
+                    let Ok(entries) = get_recent_projects() else {
+                        // TODO: do something smarter
+                        return rofi_mode::Action::Exit;
+                    };
+                    self.entries = entries;
+                    rofi_mode::Action::Reload
                 }
-                Entry::OpenProject(path) => {
-                    // TODO: handle potential errors
-                    open_zed(&path);
-                    return rofi_mode::Action::Exit;
+                Entry::Open(project) => {
+                    if let Err(_) = open_zed(project) {
+                        // TODO: do something smarter
+                        return rofi_mode::Action::Exit;
+                    }
+                    rofi_mode::Action::Exit
                 }
             },
             Event::Cancel { selected } => {
                 let Some(selected) = selected else {
                     return rofi_mode::Action::Exit;
                 };
-                return match self.entries[*selected] {
+                match self.entries[*selected] {
                     Entry::SearchRecents => rofi_mode::Action::Exit,
-                    Entry::OpenProject(_) => {
+                    Entry::Open(_) => {
                         self.entries = get_menu_entries();
                         rofi_mode::Action::Reload
                     }
-                };
+                }
             }
-            Event::Complete { .. } => {}
-            Event::CustomInput { .. } => {}
-            Event::CustomCommand { .. } => {}
-            Event::DeleteEntry { .. } => {}
+            _ => rofi_mode::Action::Reset,
+            // Event::Complete { .. } => {}
+            // Event::CustomInput { .. } => {}
+            // Event::CustomCommand { .. } => {}
+            // Event::DeleteEntry { .. } => {}
         }
-        rofi_mode::Action::Reload
     }
 
     fn matches(&self, line: usize, matcher: rofi_mode::Matcher<'_>) -> bool {
