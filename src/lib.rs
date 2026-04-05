6debe32bd0d9e6e9281686cd56e9d700a000ea48
wardxela <wardxela@gmail.com>
2026-04-05 13:50:33 +0300

    refactor: polish code + log errors to stderr

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,16 @@
 mod zed;
 
-use rofi_mode::{Action, Event};
+use rofi_mode::{Action, Api, Event, Mode};
 
 pub struct ZedMode<'rofi> {
     api: rofi_mode::Api<'rofi>,
     entries: Vec<String>,
 }
 
-impl<'rofi> rofi_mode::Mode<'rofi> for ZedMode<'rofi> {
+impl<'rofi> Mode<'rofi> for ZedMode<'rofi> {
     const NAME: &'static str = "zed\0";
 
-    fn init(api: rofi_mode::Api<'rofi>) -> Result<Self, ()> {
+    fn init(api: Api<'rofi>) -> Result<Self, ()> {
         let entries = zed::get_recent_projects().ok_or(())?;
         Ok(Self { api, entries })
     }
@@ -28,12 +28,13 @@ impl<'rofi> rofi_mode::Mode<'rofi> for ZedMode<'rofi> {
     }
 
     fn react(&mut self, event: Event, _input: &mut rofi_mode::String) -> Action {
-        match &event {
-            Event::Ok { selected, .. } => match zed::open_project(&self.entries[*selected]) {
-                Ok(_) => Action::Exit,
-                // TODO: report an error
-                Err(_) => Action::Exit,
-            },
+        match event {
+            Event::Ok { selected, .. } => zed::open_project(&self.entries[selected])
+                .map(|_| Action::Exit)
+                .unwrap_or_else(|err| {
+                    eprint!("{err}");
+                    Action::Exit
+                }),
             Event::Cancel { .. } => Action::Exit,
             _ => Action::Reset,
             // Event::Complete { .. } => {}
